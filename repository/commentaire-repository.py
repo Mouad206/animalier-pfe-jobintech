f75761a4bb7752dc580f89f87b03cbfabc051a60
= <=>
2026-02-27 13:29:55 +0000

    suprimmer des fichier

--- repository/commentaire-repository.py
+++
@@ -1,9 +0,0 @@
-from base_repository import BaseRepository
-
-class CommentaireRepository(BaseRepository):
-
-    def find_by_user(self, user_id):
-        pass
-
-    def find_by_evaluation(self, evaluation_id):
-        pass
